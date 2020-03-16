c43426ba73a34393baca108d4e5f10ce4389ddf1
Noo8 Cod3r <brent.s.rodriguez@gmail.com>
2020-03-16 00:41:52 -0500

    Add JS

--- scripts.js
+++ scripts.js
@@ -1,7 +1,38 @@
 const container = document.querySelector('.container')
-const seat = document.querySelectorAll('.row .seat:not(.occupied')
-const count = document.querySelector('count')
-const total = document.querySelector('total')
+const seats = document.querySelectorAll('.row .seat:not(.occupied')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
-const ticketPrice = +movieSelect.value // + changes the Type from string to number
+let ticketPrice = +movieSelect.value // + changes the Type from string to number
+
+// Update Count
+const updateCounts = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const selectedSeatsCount = selectedSeats.length
+
+  // Seats Selected * price of a Ticket
+  count.innerText = selectedSeatsCount
+  total.innerText = selectedSeatsCount * ticketPrice
+}
+
+// Movie Select (Update Price of ticket based on movie price)
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value
+  updateCounts()
+})
+
+
+
+// Select seat event
+container.addEventListener('click', e => {
+  if(
+    e.target.classList.contains('seat') && 
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected')
+
+    updateCounts()
+  }
+})   
